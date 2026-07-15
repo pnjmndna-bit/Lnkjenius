a641aac2e72b282b835021b7c866cf118628df09
pnjmndna-bit <pnjmndna@gmail.com>
2026-07-15 23:31:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -40,12 +40,14 @@ process.env.CHAT_ID;
 
 const statusData = {};
 
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended:true
-    })
-);
+app.use(express.json({
+    limit: "20mb"
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "20mb"
+}));
 
 app.use(
     express.static(
